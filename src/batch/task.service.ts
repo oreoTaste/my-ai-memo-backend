46c9f98347036dd73bf986927bf70d746fb8a149
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2025-03-07 14:32:21 +0900

    배치 발송 문구 수정

--- src/batch/task.service.ts
+++ src/batch/task.service.ts
@@ -48,7 +48,8 @@ export class TaskService {
     }
   }
 
-  @Cron(CronExpression.EVERY_DAY_AT_9AM, { timeZone: 'Asia/Seoul' })
+  // @Cron(CronExpression.EVERY_DAY_AT_9AM, { timeZone: 'Asia/Seoul' })
+  @Cron(CronExpression.EVERY_10_MINUTES, { timeZone: 'Asia/Seoul' })
   async sendDailyTodos(): Promise<void> {
     this.logger.log('[9AM] SendDailyTodos batch started');
     await this.initializeTelegramApi();
@@ -96,7 +97,7 @@ export class TaskService {
       return;
     }
 
-    const message = `[${todo.title}] ${todo.desc}`;
+    const message = `\[${todo.title}\]\n${todo.desc}`;
     try {
       const response = await this.telegramApi.post('/sendMessage', {
         chat_id: telegramId,
