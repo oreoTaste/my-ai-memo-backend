3d7fb61f5826720b81f5b19555e8d5eefd988ea2
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2025-03-08 22:26:15 +0900

    google drive 설정

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -3,11 +3,12 @@ import { FileService } from './file.service';
 import { UploadFile } from './entity/file.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FileController } from './file.controller';
+import { Code } from 'src/code/entity/code.entity';
 
 @Global()
 @Module({
     imports: [
-        TypeOrmModule.forFeature([UploadFile]),
+        TypeOrmModule.forFeature([UploadFile, Code]),
       ],
     providers: [FileService],
     controllers: [FileController],
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { DownloadFileDto, InsertFileDto, InsertFileResultDto, SearchFilesDto } from './dto/file.dto';
 import { join } from 'path';
 import * as path from 'path';
@@ -8,13 +8,68 @@ import { renameSync } from 'fs';
 import { UploadFile } from './entity/file.entity';
 import * as fs from 'fs';
 import { Response } from 'express';
+import { drive_v3, google } from 'googleapis';
+import { Code } from 'src/code/entity/code.entity';
 
 @Injectable()
 export class FileService {
+    private drive: drive_v3.Drive;
+
     constructor(
-        @InjectRepository(UploadFile)
-        private fileRepository: Repository<UploadFile>
+        @InjectRepository(UploadFile) private fileRepository: Repository<UploadFile>,
+        @InjectRepository(Code) private codeRepository: Repository<Code>
     ){}
+
+    // 환경 변수에서 인증 정보 가져오기
+    private async setOauth2Client() {
+        let codeList: Code[] = await this.codeRepository.find({ where: {codeGroup: "CC004", code: Like("GOOGLE%"), useYn: "Y"}, select: ['code', 'remark']});
+        let GOOGLE_DRIVE_CLIENT_ID = codeList.find(el => el.code == "GOOGLE_DRIVE_CLIENT_ID").remark;
+        let GOOGLE_DRIVE_CLIENT_SECRET = codeList.find(el => el.code == "GOOGLE_DRIVE_CLIENT_SECRET").remark;
+        let GOOGLE_DRIVE_REFRESH_TOKEN = codeList.find(el => el.code == "GOOGLE_DRIVE_REFRESH_TOKEN").remark;
+        let GOOGLE_DRIVE_FOLDER_ID = codeList.find(el => el.code == "GOOGLE_DRIVE_FOLDER_ID").remark;
+
+        const oauth2Client = new google.auth.OAuth2(
+            GOOGLE_DRIVE_CLIENT_ID,
+            GOOGLE_DRIVE_CLIENT_SECRET,
+            'https://oreotaste.duckdns.org', // redirect URI는 실제 사용 시 맞게 설정
+        );
+
+        oauth2Client.setCredentials({
+            refresh_token: GOOGLE_DRIVE_REFRESH_TOKEN,
+        });
+
+        this.drive = google.drive({
+            version: 'v3',
+            auth: oauth2Client,
+        });
+        return GOOGLE_DRIVE_FOLDER_ID;
+    }
+
+    // Google Drive에 파일 업로드
+    private async uploadToGoogleDrive(file: Express.Multer.File, fileName: string) {
+        console.log('uploadToGoogleDrive');
+        let folderId = await this.setOauth2Client();
+        console.log('this.drive.permissions : ' + JSON.stringify(this.drive.permissions));
+        console.log('folderId : ' + folderId);
+        const bufferStream = require('stream').PassThrough();
+        bufferStream.end(file.buffer);
+    
+        const response = await this.drive.files.create({
+            requestBody: {
+                name: fileName,
+                parents: [folderId],
+            },
+            media: {
+                mimeType: file.mimetype,
+                body: bufferStream,
+            },
+            fields: 'id',
+        });
+    
+        console.log('response : ' + JSON.stringify(response));
+        return response.data.id;
+    }
+
 
     public getRealFileName(savedFileName: string) {
         let match = savedFileName.match(/[^_]+_[^_]+_(.*)/);
@@ -115,6 +170,13 @@ export class FileService {
 
             // 파일 이동 (임시 디렉터리 -> 실제 저장 디렉터리)
             renameSync(uploadFile.path, targetPath);
+
+            try {
+                // 구글 드라이브에 저장
+                await this.uploadToGoogleDrive(uploadFile, uploadFile.filename);
+            } catch(e) {
+                console.error(`구글드라이브 저장중 오류 ${e}`);
+            }
 
             // 파일 정보 등록
             await this.saveFile(insertId, {fileFrom, fileName: targetPath, seq: memoSeq});
