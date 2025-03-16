0c5a14c6b57692cd270fba7725999cf1485f6574
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2025-03-16 23:51:51 +0900

    테이블 컬럼명 변경

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -7,31 +7,31 @@ import { Entity, Column, PrimaryGeneratedColumn, Index } from 'typeorm';
 @Index('Y_USER_IDX1', ['loginId'], {unique: true})
 @Index('Y_USER_IDX2', ['name', 'loginId'], {unique: false})
 export class User extends CommonEntity{
-  @PrimaryGeneratedColumn({zerofill: true, primaryKeyConstraintName: "Y_USER_PK"})
+  @PrimaryGeneratedColumn({primaryKeyConstraintName: "Y_USER_PK", name: "id"})
   @IsNumber()
   @ApiProperty()
   id: number;
 
-  @Column({type: 'varchar2', length: 30})
+  @Column({type: 'varchar2', length: 30, name: "loginId"})
   @Length(5,30)
   @IsString()
   @IsNotEmpty({message: "아이디를 입력해주세요"})
   @ApiProperty()
   loginId: string;
 
-  @Column()
+  @Column({name: "name"})
   @Length(2,100)
   @IsString()
   @IsNotEmpty({message: "이름을 입력해주세요"})
   @ApiProperty()
   name: string;
 
-  @Column({ default: true })
+  @Column({ default: true, name: "isActive"})
   @IsBoolean()
   @ApiProperty()
   isActive: boolean;
 
-  @Column()
+  @Column({name: "password"})
   @Length(6,30)
   // @IsStrongPassword({minLength:5, minLowercase:1, minUppercase:1, minNumbers:1, minSymbols:1})
   @IsString()
@@ -39,9 +39,9 @@ export class User extends CommonEntity{
   @ApiProperty()
   password: string;
 
-  @Column({nullable: true, type: 'varchar2', length: 1, default: "N"})
-  adminYn: string;
+  @Column({nullable: true, type: 'varchar2', length: 1, default: "N", name: "adminYn"})
+  adminYn: 'Y' | 'N';
 
-  @Column({nullable: true, type: 'varchar2', length: 10})
+  @Column({nullable: true, type: 'varchar2', length: 10, name: "telegramId"})
   telegramId: string;
 }
