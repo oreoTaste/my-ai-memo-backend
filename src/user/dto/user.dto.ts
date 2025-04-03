db2d500cc19619a5a48cadd1c6714f3d636159f1
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2025-04-03 09:11:13 +0900

    SignInUserDto추가

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -6,6 +6,9 @@ import { CommonResultDto } from "src/common/dto/common.dto";
 export class LoginUserDto extends PickType(User, ['loginId', 'password'] as const){}
 export class InsertUserDto extends PickType(User, ['loginId', 'password', 'name'] as const){}
 export class SearchUserDto extends PartialType(PickType(User, ['loginId', 'name'] as const)){}
+export class AuthUserDto extends PickType(User, ['id', 'loginId', 'name', 'isActive', 'createdAt', 'adminYn']){}
+export class SignInUserDto extends PickType(User, ['id', 'loginId', 'name', 'isActive', 'createdAt', 'adminYn', 'telegramId']){}
+
 export class InsertUserResultDto extends CommonResultDto {
     constructor(user: User, result?: boolean, message?: string[]) {
         super(result == undefined ? true : result
@@ -50,5 +53,3 @@ export class ListUserResultDto extends CommonResultDto {
     @IsOptional()
     users?: User[];
 }
-
-export class AuthUserDto extends PickType(User, ['id', 'loginId', 'name', 'isActive', 'createdAt', 'adminYn']){}
